bf8502036e1fad7944208d403489e3c7b26c31dc
ANJALI-80 <rathodanjali7979@gmail.com>
2023-03-05 06:55:07 -0800

    inliner

---
+++ pr_2/1_pass function.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int sum (int a[],int n)
+{
+	int i,s=0;
+	for(i=0;i<n;i++)
+	{
+		cout << "a[" << i << "] : ";
+		cin >> a[i];
+		s += a[i];	
+	}
+	return s;
+}
+void ave(int a,int n)
+{
+	//cout << " average is : " << a/n ; 
+	
+	cout<<"sum is : "<<a<<endl<<"avrage is :" <<a/n;
+}
+
+int main()
+{
+	int n;
+	cout << "Enter n : ";
+	cin >> n;
+	int a[n];
+	ave(sum(a,n),n);
+}
